522bc397584b3e8aa3c5c6f997f0ddc7e1538932
Miguel Batres <btrsco@Miguels-MacBook-Pro-2021.local>
2022-04-10 12:00:36 -0700

    Implement password generator field styles, ui and logic

--- resources/js/field.js
+++ resources/js/field.js
@@ -2,8 +2,8 @@ import IndexField from './components/IndexField'
 import DetailField from './components/DetailField'
 import FormField from './components/FormField'
 
-Nova.booting((app, store) => {
-  app.component('index-password-generator', IndexField)
-  app.component('detail-password-generator', DetailField)
-  app.component('form-password-generator', FormField)
-})
+Nova.booting( ( app, store ) => {
+    app.component( 'index-password-generator', IndexField )
+    app.component( 'detail-password-generator', DetailField )
+    app.component( 'form-password-generator', FormField )
+} )
